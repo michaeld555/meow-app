d461729a3acf48b314f288a203776a2000b371bc
Michael Douglas <usermich999@gmail.com>
2022-08-17 17:33:12 -0300

    #1

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,20 @@
+export default {
+ colors: {
+    black_0: '#000000',
+    black_1: '#0f0f0f',
+    black_2: '#0f0f0fe0',
+    yellow: {
+      500: '#FFAC00',
+    },
+    orange: '#E65C00',
+    blue: '#2C3E50',
+    white: '#fff',
+    gray: '#F7F9FA',
+    gray_normal: '#191919',
+    gray_dark: '#222',
+    pink: '#50B4F2',//azul claro
+    purple_dark: '#366CD9', //azul medio
+    purple_normal: '#2944D9',//azul escuro
+    purple_light: '819BFD',
+  },
+};
